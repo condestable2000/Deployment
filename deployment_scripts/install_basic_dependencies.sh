549d85bc80a331901d597b9ee3ecad3ca2c412c1
condestable2000 <condestable2000@users.noreply.github.com>
2023-12-31 16:12:30 +0100

    Update install_basic_dependencies.sh

--- deployment_scripts/install_basic_dependencies.sh
+++ deployment_scripts/install_basic_dependencies.sh
@@ -3,6 +3,6 @@
 # These are the dependencies that both webapp and grading agents need
 
 sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
-sudo yum install -y git gcc libtidy python36-pip python36 python3-devel zeromq-devel
-sudo pip3.6 install --upgrade pip
+sudo yum install -y git gcc libtidy python-pip python python3-devel zeromq-devel
+sudo pip install --upgrade pip
 bash "$DEPLOYMENT_HOME/deployment_scripts/install_docker.sh"

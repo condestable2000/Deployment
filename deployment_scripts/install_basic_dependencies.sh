411d427989d934b8c6d9ad22382b4540917dda1e
condestable2000 <condestable2000@users.noreply.github.com>
2023-12-31 17:07:09 +0100

    Update install_basic_dependencies.sh

--- deployment_scripts/install_basic_dependencies.sh
+++ deployment_scripts/install_basic_dependencies.sh
@@ -5,8 +5,8 @@
 sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
 sudo yum install -y git gcc python-pip python python3-devel wget
 sudo pip install --upgrade pip
-wget https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libtidy-5.4.0-1.el7.x86_64.rpm
-rpm -Uvh libtidy-5.4.0-1.el7.x86_64.rpm
-wget https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/z/zeromq-devel-4.1.4-6.el7.x86_64.rpm
-rpm -Uvh zeromq-devel-4.1.4-6.el7.x86_64.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libtidy-5.4.0-1.el7.x86_64.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libsodium-1.0.18-1.el7.x86_64.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/z/zeromq-4.1.4-6.el7.x86_64.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/z/zeromq-devel-4.1.4-6.el7.x86_64.rpm
 bash "$DEPLOYMENT_HOME/deployment_scripts/install_docker.sh"

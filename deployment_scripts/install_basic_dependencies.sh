129c4cbf1b6189b76e9056c8fc6b6e4a84f6e857
condestable2000 <condestable2000@users.noreply.github.com>
2023-12-31 17:10:21 +0100

    Update install_basic_dependencies.sh

--- deployment_scripts/install_basic_dependencies.sh
+++ deployment_scripts/install_basic_dependencies.sh
@@ -6,6 +6,7 @@ sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
 sudo yum install -y git gcc python-pip python python3-devel wget
 sudo pip install --upgrade pip
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libtidy-5.4.0-1.el7.x86_64.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/openpgm-5.2.122-2.el7.x86_64.rpm
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/l/libsodium-1.0.18-1.el7.x86_64.rpm
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/z/zeromq-4.1.4-6.el7.x86_64.rpm
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/z/zeromq-devel-4.1.4-6.el7.x86_64.rpm

9d1660d67288b5032bc2a40a7ed48f1648277e32
condestable2000 <condestable2000@users.noreply.github.com>
2023-12-31 17:33:05 +0100

    Update install_node.sh

--- deployment_scripts/install_node.sh
+++ deployment_scripts/install_node.sh
@@ -8,5 +8,6 @@ if [ "$EUID" -ne 0 ]
 fi
 
 sudo yum install  -y epel-release
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+sudo yum install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+sudo yum install nsolid -y | sudo bash -
 sudo yum install -y nodejs

0345cea42646e35aa3ff63db7e5694b513b87d28
condestable2000 <condestable2000@users.noreply.github.com>
2024-01-01 21:18:59 +0100

    Update install_node.sh

--- deployment_scripts/install_node.sh
+++ deployment_scripts/install_node.sh
@@ -11,6 +11,5 @@ sudo yum install  -y epel-release
 sudo yum install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 
 #Solucionar error de GPG
-sudo yum install nsolid -y -nogpgcheck #sudo yum install nsolid -y | sudo bash -
-
-sudo yum install -y nodejs
+sudo yum install nsolid -y --nogpgcheck #sudo yum install nsolid -y | sudo bash -
+sudo yum install nodejs -y --nogpgcheck
